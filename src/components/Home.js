4c1c96b540fb723b7f1c68f4d85745d0d84d0819
John Larson <35274970+waaade@users.noreply.github.com>
2023-04-06 20:48:31 -0600

    Add workoutview table and page routing

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2> TODO. Maybe show something like, Hello User, you have ? workouts this week</h2>
+
+        </div>
+    )
+}
