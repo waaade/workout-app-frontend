81a2ccd02339c33c871beaadf4ecde2d91fc2f6f
Gavin Liburd <GavinLiburd901@gmail.com>
2023-04-09 22:47:07 -0400

    Updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import WorkoutApi from '../apis/WorkoutApi';
 
 
-const Home = (token) => { 
+const Home = (token) => {
+    const [userWorkouts, setUserWorkouts] = useState([]);
+    const [currentUW, setCurrentUW] = useState(0);
+
+    // Get a list of all the userWorkouts
+    useEffect(() => {
+        
+        WorkoutApi.getAllUserWorkouts(setUserWorkouts, token)
+
+    }, [])
+
+    // After the list of userWorkouts is collected, count the workouts made by the currently signed in user
+    useEffect(() => {
+
+        const currentUsersUserWorkouts = userWorkouts.filter((uw) => uw.userId.id === token.userId);
+        console.log(currentUsersUserWorkouts.length)
+        if (currentUsersUserWorkouts.length > 0) {
+            setCurrentUW(currentUsersUserWorkouts.length);
+        }
+
+    }, [userWorkouts])
+
     return (
-        <div>
-            <h2>Welcome</h2>
-            <p>Maybe we could show something like, "Hello User, you have ? workouts this week"?</p>
+        <div style={{marginTop: '10px'}}>
+            <h2>Welcome, {token.name} </h2>
+            <h4>{token.name}, you have a total of {currentUW} workouts. {currentUW ? 'Keep up the good work!' : 'You can do better than that!'}</h4>
         </div>
     )
 }
